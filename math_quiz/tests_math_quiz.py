0a0ed44eb1eceaedec59403bd3101ad81bc89edd
Sarah Naber <sarah.naber@fau.de>
2023-11-13 17:51:47 +0100

    Modified version of the tests_math_quiz.py file with unit test for the earlier modified file math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,34 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+import random
+from math_quiz import get_integer, get_sign, calculation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_function_get_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = get_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_function_get_sign(self):
+        random.seed(42) # everytime the same sign gets selected if the methodes works correctly
+        sign = get_sign()
+        self.assertTrue(sign == '*')
 
-    def test_function_C(self):
+    def test_function_calculation(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (7, 8, '*', '7 * 8', 56),
+                (3, 1, '-', '3 - 1', 2),
             ]
-
+            # Test if each case gets calculated correctly
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                problem, answer = calculation(num1, num2, operator)
+                self.assertTrue(problem == expected_problem and answer == expected_answer)
+                
 
 if __name__ == "__main__":
     unittest.main()
